bff926c68955fe65d7151dc3e156334dbe0ac910
shawn-mcginty-advocate <55758542+shawn-mcginty-advocate@users.noreply.github.com>
2020-07-11 20:10:54 -0400

    Update EF_Models.cs

--- MVC_EF_Start/Models/EF_Models.cs
+++ MVC_EF_Start/Models/EF_Models.cs
@@ -1,40 +1,27 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MVC_EF_Start.Models
 {
-  public class Company
+  public class Course
   {
     [Key]
-    public string symbol { get; set; }
-    public string name { get; set; }
-    public string date { get; set; }
-    public bool isEnabled { get; set; }
-    public string type { get; set; }
-    public string iexId { get; set; }
-    public List<Quote> Quotes { get; set; }
+    public string CourseName { get; set; }
+    public int CourseId { get; set; }
+
   }
 
-  public class Quote
+  public class Student
   {
-    public int QuoteId { get; set; }
-    public string date { get; set; }
-    public float open { get; set; }
-    public float high { get; set; }
-    public float low { get; set; }
-    public float close { get; set; }
-    public int volume { get; set; }
-    public int unadjustedVolume { get; set; }
-    public float change { get; set; }
-    public float changePercent { get; set; }
-    public float vwap { get; set; }
-    public string label { get; set; }
-    public float changeOverTime { get; set; }
-    public string symbol { get; set; }
+    public int StudentId { get; set; }
+    public string StudentName { get; set; }
   }
 
-  public class ChartRoot
+  public class Enrollment
   {
-    public Quote[] chart { get; set; }
+    public int EnrollmentId { get; set; }
+    public string Grade { get; set; }
+    public Course Course { get; set; }
+    public Student Student { get; set; }
   }
 }
