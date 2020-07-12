fcc5919119ac05b83fe0af1d875bf43a4fa30875
shawn-mcginty-advocate <55758542+shawn-mcginty-advocate@users.noreply.github.com>
2020-07-11 20:09:59 -0400

    Update DatabaseExampleController.cs

--- MVC_EF_Start/Controllers/DatabaseExampleController.cs
+++ MVC_EF_Start/Controllers/DatabaseExampleController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,74 +25,72 @@ namespace MVC_EF_Start.Controllers
 
     public async Task<ViewResult> DatabaseOperations()
     {
-      // CREATE operation
-      Company MyCompany = new Company();
-      MyCompany.symbol = "MCOB";
-      MyCompany.name = "ISM";
-      MyCompany.date = "ISM";
-      MyCompany.isEnabled = true;
-      MyCompany.type = "ISM";
-      MyCompany.iexId = "ISM";
+            // CREATE operation
+            Course Course = new Course();
+            Course.CourseId = 6225;
+            Course.CourseName = "ISM6225";
 
-      Quote MyCompanyQuote1 = new Quote();
-      //MyCompanyQuote1.EquityId = 123;
-      MyCompanyQuote1.date = "11-23-2018";
-      MyCompanyQuote1.open = 46.13F;
-      MyCompanyQuote1.high = 47.18F;
-      MyCompanyQuote1.low = 44.67F;
-      MyCompanyQuote1.close = 47.01F;
-      MyCompanyQuote1.volume = 37654000;
-      MyCompanyQuote1.unadjustedVolume = 37654000;
-      MyCompanyQuote1.change = 1.43F;
-      MyCompanyQuote1.changePercent = 0.03F;
-      MyCompanyQuote1.vwap = 9.76F;
-      MyCompanyQuote1.label = "Nov 23";
-      MyCompanyQuote1.changeOverTime = 0.56F;
-      MyCompanyQuote1.symbol = "MCOB";
+            Student Student = new Student();
+            Student.StudentId = 9275;
+            Student.StudentName = "Shawn McGinty";
 
-      Quote MyCompanyQuote2 = new Quote();
-      //MyCompanyQuote1.EquityId = 123;
-      MyCompanyQuote2.date = "11-23-2018";
-      MyCompanyQuote2.open = 46.13F;
-      MyCompanyQuote2.high = 47.18F;
-      MyCompanyQuote2.low = 44.67F;
-      MyCompanyQuote2.close = 47.01F;
-      MyCompanyQuote2.volume = 37654000;
-      MyCompanyQuote2.unadjustedVolume = 37654000;
-      MyCompanyQuote2.change = 1.43F;
-      MyCompanyQuote2.changePercent = 0.03F;
-      MyCompanyQuote2.vwap = 9.76F;
-      MyCompanyQuote2.label = "Nov 23";
-      MyCompanyQuote2.changeOverTime = 0.56F;
-      MyCompanyQuote2.symbol = "MCOB";
+            Enrollment Enrollment = new Enrollment();
+            Enrollment.Course = Course;
+            Enrollment.EnrollmentId = 3234;
+            Enrollment.Student = Student;
+            Enrollment.Grade = "A";
 
-      dbContext.Companies.Add(MyCompany);
-      dbContext.Quotes.Add(MyCompanyQuote1);
-      dbContext.Quotes.Add(MyCompanyQuote2);
+      dbContext.Courses.Add(Course);
+      dbContext.Students.Add(Student);
+      dbContext.Enrollments.Add(Enrollment);
 
       dbContext.SaveChanges();
       
       // READ operation
-      Company CompanyRead1 = dbContext.Companies
-                              .Where(c => c.symbol == "MCOB")
+      Enrollment Enrollment1 = dbContext.Enrollments
+                              .Where(e => e.EnrollmentId == 3234)
                               .First();
 
-      Company CompanyRead2 = dbContext.Companies
-                              .Include(c => c.Quotes)
-                              .Where(c => c.symbol == "MCOB")
+      Enrollment Enrollment2 = dbContext.Enrollments
+                              .Include(e => e.Course)
+                              .Where(e => e.Course.CourseId == Course.CourseId)
                               .First();
 
-      // UPDATE operation
-      CompanyRead1.iexId = "MCOB";
-      dbContext.Companies.Update(CompanyRead1);
+            // UPDATE operation
+            Enrollment1.Course.CourseId = 6225;
+            dbContext.Enrollments.Update(Enrollment1);
       //dbContext.SaveChanges();
       await dbContext.SaveChangesAsync();
       
       // DELETE operation
-      dbContext.Companies.Remove(CompanyRead1);
-      await dbContext.SaveChangesAsync();
+      //dbContext.Enrollments.Remove(Enrollment1);
+      //await dbContext.SaveChangesAsync();
 
       return View();
     }
+
+    //public ViewResult LINQOperations()
+    //{
+    //  Course Course1 = dbContext.Courses
+    //         .Where(c => c.CourseName=="ISM6225")
+    //         .First();
+
+    //  Enrollment Enrollment1 = dbContext.Enrollments
+    //         .Where(e => e.EnrollmentId == 3234)
+    //         .First();
+
+    //  Enrollment Enrollment2 = dbContext.Enrollments
+    //         .Include(e => e.Course)
+    //         .Where(e => e.Course.CourseId == Course1.CourseId)
+    //         .First();
+
+    //        Student Student1 = dbContext.Enrollments
+    //              .Include(e => e.Student)
+    //              .Where(e => e.Student = Student)
+    //              .FirstOrDefault()
+    //              .Students
+    //              .FirstOrDefault();
+     // return View();
+    }
+
   }
-}
