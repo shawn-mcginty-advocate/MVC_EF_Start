7de37390ceda0652c0455d2160708af23270023a
subsonar704 McGinty <spmcginty1030@gmail.com>
2020-07-18 08:49:54 -0400

    LINQ exercise

--- MVC_EF_Start/Controllers/DatabaseExampleController.cs
+++ MVC_EF_Start/Controllers/DatabaseExampleController.cs
@@ -9,22 +9,22 @@ using MVC_EF_Start.Models;
 
 namespace MVC_EF_Start.Controllers
 {
-  public class DatabaseExampleController : Controller
-  {
-    public ApplicationDbContext dbContext;
-
-    public DatabaseExampleController(ApplicationDbContext context)
+    public class DatabaseExampleController : Controller
     {
-      dbContext = context;
-    }
+        public ApplicationDbContext dbContext;
 
-    public IActionResult Index()
-    {
-      return View();
-    }
+        public DatabaseExampleController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
 
-    public async Task<ViewResult> DatabaseOperations()
-    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ViewResult> DatabaseOperations()
+        {
             // CREATE operation
             Course Course = new Course();
             Course.CourseId = 6225;
@@ -40,57 +40,58 @@ namespace MVC_EF_Start.Controllers
             Enrollment.Student = Student;
             Enrollment.Grade = "A";
 
-      dbContext.Courses.Add(Course);
-      dbContext.Students.Add(Student);
-      dbContext.Enrollments.Add(Enrollment);
+            dbContext.Courses.Add(Course);
+            dbContext.Students.Add(Student);
+            dbContext.Enrollments.Add(Enrollment);
 
-      dbContext.SaveChanges();
-      
-      // READ operation
-      Enrollment Enrollment1 = dbContext.Enrollments
-                              .Where(e => e.EnrollmentId == 3234)
-                              .First();
+            dbContext.SaveChanges();
 
-      Enrollment Enrollment2 = dbContext.Enrollments
-                              .Include(e => e.Course)
-                              .Where(e => e.Course.CourseId == Course.CourseId)
-                              .First();
+            // READ operation
+            Enrollment Enrollment1 = dbContext.Enrollments
+                                    .Where(e => e.EnrollmentId == 3234)
+                                    .First();
+
+            Enrollment Enrollment2 = dbContext.Enrollments
+                                    .Include(e => e.Course)
+                                    .Where(e => e.Course.CourseId == Course.CourseId)
+                                    .First();
 
             // UPDATE operation
             Enrollment1.Course.CourseId = 6225;
             dbContext.Enrollments.Update(Enrollment1);
-      //dbContext.SaveChanges();
-      await dbContext.SaveChangesAsync();
-      
-      // DELETE operation
-      //dbContext.Enrollments.Remove(Enrollment1);
-      //await dbContext.SaveChangesAsync();
+            //dbContext.SaveChanges();
+            await dbContext.SaveChangesAsync();
 
-      return View();
+            // DELETE operation
+            //dbContext.Enrollments.Remove(Enrollment1);
+            //await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        public ViewResult LINQOperations()
+        {
+            Course Course1 = dbContext.Courses
+                   .Where(c => c.CourseName == "ISM6225")
+                   .First();
+
+            Enrollment Enrollment1 = dbContext.Enrollments
+                   .Where(e => e.EnrollmentId == 3234)
+                   .First();
+
+            Enrollment Enrollment2 = dbContext.Enrollments
+                   .Include(e => e.Course)
+                   .Where(e => e.Course.CourseId == Course1.CourseId)
+                   .First();
+
+            //    Student Student1 = dbContext.Enrollments
+            //          .Include(e => e.Student)
+            //          .Where(e => e.Student = Student)
+            //          .FirstOrDefault()
+            //          .Students
+            //          .FirstOrDefault();
+            return View();
+        }
+
     }
-
-    //public ViewResult LINQOperations()
-    //{
-    //  Course Course1 = dbContext.Courses
-    //         .Where(c => c.CourseName=="ISM6225")
-    //         .First();
-
-    //  Enrollment Enrollment1 = dbContext.Enrollments
-    //         .Where(e => e.EnrollmentId == 3234)
-    //         .First();
-
-    //  Enrollment Enrollment2 = dbContext.Enrollments
-    //         .Include(e => e.Course)
-    //         .Where(e => e.Course.CourseId == Course1.CourseId)
-    //         .First();
-
-    //        Student Student1 = dbContext.Enrollments
-    //              .Include(e => e.Student)
-    //              .Where(e => e.Student = Student)
-    //              .FirstOrDefault()
-    //              .Students
-    //              .FirstOrDefault();
-     // return View();
-    }
-
-  }
+}
